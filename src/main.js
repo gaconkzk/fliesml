754f88cf49837ac75961fb98e422ad368609b272
gaconkzk <gaconkzk@gmail.com>
2019-08-23 16:19:50 +0700

    working on page

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const mountApp = riot.component(mainApp)
 
 import 'normalize.css'
 // import 'bulma/css/bulma.min.css'
-import '@/style/bulmaswatch.min.css'
+// import '@/style/bulmaswatch.min.css'
 import '@/style/global.styl'
 
 import '@mojs/core'
